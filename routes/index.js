a8f43c3fac606c39c0080562a3fc5b8ab1f53175
Kyle <kyleskabelund@hotmail.com>
2016-03-29 14:39:22 -0600

    added login file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var crypto  = require('crypto');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Today I Learned' });
 });
+router.get('/login', function(req,res,next){
+  res.render('login',{title: 'Login'});
+});
 
+router.post('/login',function(req,res,next){
+  var sum = crypto.createHash('sha1');
+
+  req.db.driver.execQuery(
+    'SELECT * FROM users WHERE email = ?; ',
+    [req.body.email],
+    function(err,data){
+      if(err){console.log(err)}
+
+    sum.update(req.body.password);
+    var hashed = sum.digest('hex');
+
+    if(hashed === data[0].password)
+    {
+      res.cookie('username',data[0].name);
+      res.redirect('/entries/');
+    }
+    else
+    {
+      res.redirect('/login');
+    }
+  }
+  );
+});
 module.exports = router;
